e3044f8975b9bf48ed495f38301edd0d1adf7a5c
Najma <najma.a16@gmail.com>
2023-01-09 16:42:29 -0600

    final backend redo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,89 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/day" do
+    day = Day.all
+    day.to_json
   end
+
+  get '/tasks/:id' do
+    task = Task.find(params[:id])
+    task.to_json(include: :day)
+  end
+
+  get '/tasks' do
+    tasks = Task.all
+    tasks.to_json(include: :day)
+  end
+
+  post '/tasks' do
+    tasks = Task.create(
+      name: params[:name],
+      day_id: params[:day_id]
+    )
+    tasks.to_json(include: :day)
+  end
+
+  patch '/tasks/:id' do
+    task = Task.find(params[:id])
+    task.update(
+      name: params[:name],
+      day_id: params[:day_id]
+    )
+    task.to_json(include: :day)
+  end
+
+  delete '/tasks/:id' do
+    task = Task.find(params[:id])
+    task.destroy
+    task.to_json(include: :day)
+  end
+
+  # post '/tasks' do
+  #   day = Day.find_by(day_of_week: params[:day][:day].downcase)
+  #   category = Category.find_by(urgent: params[:category])
+  #   task_to_add = Task.all.includes(:category, :day).create(
+  #     description: params[:description][:formTask],
+  #     category: category,
+  #     day: day
+  #   )
+  #   # binding.pry
+  #   task_to_add.to_json(:include => [:category, :day])
+  # end
+
+# get '/days' do
+  #   # binding.pry
+  #   day = Day.all
+  #   day.to_json
+  # end
+
+  # get '/days/:id' do
+  #   day = Day.find(params[:id])
+  #   day.to_json
+  # end
+
+  # post '/days/:id' do
+  #   day_to_add = Day.create(
+  #     day_of_week: params[:day_of_week],
+  #     # task_id: params[:task_id],
+  #     # category_id: params[:category_id]
+  #   )
+  #   day_to_add.to_json
+  # end
+
+  # patch '/days/:id' do
+  #   day_to_update = Day.find(params[:id])
+  #   day_to_update.update(
+  #     day_of_week: params[:day_of_week],
+  #     # category_id: params[:category_id],
+  #     # task_id: params[:task_id]
+  #   )
+  #   day_to_update.to_json
+  # end
+
+  # delete '/days/:id' do
+  #   day_to_delete = Day.find(params[:id])
+  #   day_to_delete.delete
+  # end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+d1 = Day.create(day_of_week: "Monday")
+d2 = Day.create(day_of_week: "Tuesday")
+d3 = Day.create(day_of_week: "Wednesday")
+d4 = Day.create(day_of_week: "Thursday")
+d5 = Day.create(day_of_week: "Friday")
+d6 = Day.create(day_of_week: "Saturday")
+d7 = Day.create(day_of_week: "Sunday")
+
+t1 = Task.create(name: 'Wash the dishes', day_id: d1.id)
+t2 = Task.create(name: 'Water the plants', day_id: d2.id)
 
 puts "✅ Done seeding!"
